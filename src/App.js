b35dfca3af2f3e017c736382ad8b8d21c5d0d9b1
Rohan <rohanmisal89@gmail.com>
2024-03-17 15:23:42 +0530

    routes config

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React from 'react'
-import Navbar from './components/Navbar'
 import { Container } from 'react-bootstrap'
 import Footer from './components/Footer'
-
+import Header from './components/Navbar'
+import { HashRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Signup from './components/pages/Signup'
+import Login from './components/pages/Login'
+import Events from './components/pages/Events.js'
 
 export default function App() {
   return (
     <>
-      <div>
-        <Navbar/>
+    <Router>
+      <Header/>
+        <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+        </Routes>
+        <Routes>
+        <Route exact path='/login' element={<Login/>}></Route>
+        </Routes>
+        <Routes>
+        <Route exact path='/signup' element={<Signup/>}></Route>
+        </Routes>
+        <Routes>
+        <Route exact path='/events' element={<Events/>}></Route>
+        </Routes>
+        
+    </Router>
+      {/* <div>
+        <Header/>
         <Container>
           <h1>Home</h1>
         </Container>
         <Footer/>
-      </div>
+      </div> */}
     </>
   )
 }
